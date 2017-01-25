c6eddec738b0c827feb34ab044bae744371dd701
Olivier Martineau <omartino@in2p3.fr>
2017-01-25 09:37:33 +0100

    Added .gitignore folder and updated crflux.m with up-to-date version.

--- crflux.m
+++ crflux.m
@@ -1,53 +1,41 @@
 function []=crflux(threshold)
 % Expected CR flux above thresold (in eV)
 % OMH 20/10/09
-threshold=sscanf(threshold,'%f')%SL
-
-index = 2.7;
-maxev = 1e20;
-thresgev = threshold*1e-9;  %GeV
-threstev = threshold*1e-12;  %TeV
-maxtev = maxev*1e-12;
 
 delta_days = 1; %days
+mindeg = 89;
+maxdeg = 90;
+%surf = 5000; %m2
+%surf = 800*200; %TREND-15
+%surf = 2700*500; %TREND-50
+%surf = 300e6;
+%surf = 17588; % GRAND-proto 
+%surf = 2500*600; % GRAND-proto 
+%surf = (50).^2
+%surf = 220e3*270e3; % GRAND60000
+surf = 200000*1e6; % GRAND200000
+%surf = 3.14*200*200;
+%surf = 3.14*500*500;  % LHASSO
+
 delta_t = delta_days*3600*24;  %s
 
+index1 = 3.26;  % Below ankle (10^18.6eV)
+index2 = 2.65;  % Above ankle (10^18.6eV)
+eankle = 10.^18.6;
+emax = 10.^19.5;
 pi = 3.1415927;
-mindeg = 40;
-maxdeg = 70;
 theta_min = mindeg * pi/180;
 theta_max = maxdeg * pi/180;  % degrees
 th = theta_min:0.0001:theta_max;
 
-%surf = 5000; %m2
-%surf = 800*200; %TREND-15
-%surf = 150*150; %TREND-50
-%surf = 2700*500; %TREND-50
-%surf = 1.3e7;
-%surf = 2500*600; % GRAND-proto 
-surf = (50).^2
-%surf = 220e3*270e3; % GRAND
-%surf = 3.14*200*200;
-%surf = 3.14*500*500;  % LHASSO
-
-disp(sprintf('Erange = %3.1e - %3.1e eV, duration = %3.1f days, ground surface = %3.1f m2',threshold,maxev,delta_days,surf))
-disp(sprintf('Angular range: %3.1f - %3.1f deg -> solid angle = %3.1f sr, effective area = %3.1f m2.',mindeg,maxdeg,2*pi*trapz(th,sin(th)),surf*trapz(th,cos(th))))
+disp(sprintf('Erange = %3.1e - %3.1e eV, duration = %3.1f days, ground surface = %3.1f m2',threshold,emax,delta_days,surf))
 
 solid_angle = 2*pi*trapz(th,sin(th));
+disp(sprintf('Angular range: %3.1f - %3.1f deg -> solid angle = %3.1f sr, effective area = %3.1f m2sr.',mindeg,maxdeg,solid_angle,surf*solid_angle))
+J1 = 1.06e29;  % Computed from http://arxiv.org/pdf/1511.07510.pdf
+integ_flux1 = J1*delta_t*surf*solid_angle*(threshold^(-index1+1)-eankle^(-index1+1))/(index1-1)
+J2 = 4.5e17;  % Computed from http://arxiv.org/pdf/1511.07510.pdf
+integ_flux2 = J2*delta_t*surf*solid_angle*(eankle^(-index2+1)-emax^(-index2+1))/(index2-1)
 
-a=1.8e4;
-integ_flux_norm1 = a*delta_t*surf*thresgev^(-index+1)/(index-1)
-%integ_flux_norm1 = a*delta_t*surf*solid_angle*thresgev^(-index+1)/(index-1)
-J0=8.98e-2;
-%integ_flux_norm2 = J0*delta_t*surf*solid_angle*threstev^(-index+1)/(index-1)
-integ_flux_norm2 = J0*delta_t*surf*threstev^(-index+1)/(index-1)
-
-% e=[1e17 1.8e17 3.5e17 6e17 9e17 1.7e18];
-% etev=e*1e-12;
-% surfeff= [0 100 2000 20000 40000 100000]; %astro-ph 1106.1164v2
-% energ=trapz(etev,surfeff.*etev.^(-index));
-% rice_flux = J0*delta_t*solid_angle*energ;
-% 
-% disp(sprintf('Expected CR-background flux in RICE = %3.3e events.',rice_flux));
-
+integ_flux_tot = integ_flux1+integ_flux2
 
